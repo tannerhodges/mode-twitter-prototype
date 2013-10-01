7f4c386c58e251218a76d5c6eacae9bf1d2a26e4
Tanner Hodges <austintannerhodges@gmail.com>
2013-10-01 14:30:16 -0400

    Complete update to Twitter API 1.1
    In a previous commit, OAuth was said to be removed from index.php but
    was not actually. index.php now only consists of an include for
    html.inc.
    
    Test code has also been removed from html.inc. HTML ough to be further
    restructured for simplicity (in light of both JS and CSS).
    
    Application is now successfully updated to Twitter API 1.1.

--- index.php
+++ index.php
@@ -1,30 +1,4 @@
 <?php
 
-// Load required lib files
-require_once('twitteroauth/twitteroauth.php');
-require_once('config.php');
-
-// Prepare a connection using a preset token
-function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
-  $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $oauth_token, $oauth_token_secret);
-  return $connection;
-}
-
-// Open connection
-$connection = getConnectionWithAccessToken(OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
-// Save connection to session for use in includes files
-$_SESSION['tweetoes_connection'] = $connection;
-
-// Pull sample content from Twitter
-$content = $connection->get("search/tweets",
-	array(
-		'q' => 'twitter',
-		'lang' => 'en',
-		'count' => '5',
-		'result_type' => 'mixed',
-		'include_entities' => 'true'
-	)
-);
-
 /* Include HTML to display on the page */
 include('html.inc');
