d79c5f1785a1cca9f6b1637db43f5161b7b33004
Tanner Hodges <austintannerhodges@gmail.com>
2013-10-01 12:33:57 -0400

    Set session id + Save connection to session
    In order for additional PHP files to make requests to Twitter without
    opening an entirely new connection (e.g, includes requested via AJAX),
    the initial connection must be stored somewhere. This is best handled
    by session variables.
    
    In order for other PHP files to access the same session, the session id
    must be set at session_start(). Each PHP file that requires access to
    this session must open with the same id (i.e, session_id() ).
    
    Now that the session_id() is set and the OAuth connection is saved,
    twitter.php can be updated.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php session_start();
+<?php session_start( session_id() ); // Include session_id() to maintain single session
 
 // Load required lib files
 require_once('twitteroauth/twitteroauth.php');
@@ -10,7 +10,11 @@ function getConnectionWithAccessToken($oauth_token, $oauth_token_secret) {
   return $connection;
 }
  
+// Open connection
 $connection = getConnectionWithAccessToken(OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
+// Save connection to session for use in includes files
+$_SESSION['tweetoes_connection'] = $connection;
+
 $content = $connection->get("search/tweets",
 	array(
 		'q' => 'twitter',
